533bcd7c4c60e634c528d0f25f0043dd356b35d0
Hadley Callaway <hccallaway@gmail.com>
2018-07-05 13:00:47 -0400

    finished through reducer but can't play the game yet

--- game/ai.js
+++ game/ai.js
@@ -1,28 +1,28 @@
-import reducer, {move, bad} from '.'
+import reducer, { move, bad } from '.';
 
 /**
  * moves(State) -> [...Action]
- * 
+ *
  * Return an array of actions which are valid moves from the given state.
  */
-export const moves = game => [] // TODO
+export const moves = game => []; // TODO
 
 /**
  * score(game: State, move: Action) -> Number
- * 
+ *
  * Given a game state and an action, return a score for how good
  * a move the action would be for the player whose turn it is.
- * 
+ *
  * Scores will be numbers from -1 to +1. 1 is a winning state, -1
  * is state from which we can only lose.
  */
 const score = (game, move) => {
   // TODO
-}
+};
 
 /**
  * play(state: State) -> Action
- * 
+ *
  * Return the best action for the current player.
  */
-export default state => undefined // TODO
+export default state => undefined; // TODO
--- game/index.js
+++ game/index.js
@@ -1,6 +1,29 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable';
+import { createStore } from 'redux';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map();
+// board = board.setIn([1, 1], 'X');
+
+// action.type
+const MOVE = 'MOVE';
+
+export const move = ([row, col], turn) => {
+  const action = {
+    row,
+    col,
+    turn,
+    type: MOVE,
+  };
+  return action;
+};
+
+export default function reducer(state = { board: Map() }, action) {
+  switch (action.type) {
+    case MOVE:
+      return {
+        board: board.setIn([action.row, action.col], move.action.turn),
+      };
+    default:
+      return state;
+  }
 }
